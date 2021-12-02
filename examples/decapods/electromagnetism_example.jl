e32e6b5a9edeb2c3a30122c914ec5e1f25d12673
Maia Gatlin <maia.gatlin@gtri.gatech.edu>
2021-12-02 14:15:08 -0500

    working on exact solution for EM benchmark

--- examples/decapods/electromagnetism_example.jl
+++ examples/decapods/electromagnetism_example.jl
@@ -6,6 +6,7 @@ using Catlab.Present
 using Catlab.Graphics
 using Catlab.CategoricalAlgebra
 using LinearAlgebra
+using SpecialFunctions
 
 using Decapods.Simulations
 using Decapods.Examples
@@ -37,68 +38,71 @@ end
 
 diag = eq_to_diagrams(EM2D)
 to_graphviz(diag; edge_len = "1.3")
-##
 
 dwd = diag2dwd(diag)
 to_graphviz(dwd, orientation = LeftToRight)
-##
 
+# s = EmbeddedDeltaSet2D("../meshes/pipe_fine.stl")
 s = EmbeddedDeltaSet2D("../meshes/disk_1_0.stl")
+s[:point] .= s[:point] ./ 10
 sd = dual(s);
 
 # Define non-default operators (multiplication by constants)
-c = -1 * (1.2566e-6 / 8.8542e-12)
-# c = -1 * (1.2566 / 8.8542)
+# c = -1 * (1.2566e-6 / 8.8542e-12)
+c = -3e8
+
 funcs = sym2func(sd)
-
-
 funcs[:c] = Dict(:operator => c * I(ntriangles(s)), :type => MatrixFunc())
 funcs[:neg₁] = Dict(:operator => -1 * I(ne(sd)), :type => MatrixFunc())
 
-# Define initial conditions
-x = [p[1] for p in s[:point]];
 
-# # sinusoidal distribution for E-field
-# eField(x) = begin
-#     amp = sin((3) * π * (x[1]))
-#     amp * Point{3,Float32}(1, 0, 0)
-# end
+# Bessel functions
 
-# radial distribution for E-field
-r = 10
+exactE(x, y, t; m = 0, ω₀ = 0, A = 1, B = 0, c = sqrt(10), C = 1, D = 0) =
+    besselj(m, ω₀ * sqrt(x^2 + y^2)) *
+    (A * cos(c * λ[n, m+1] * t) + B * sin(c * λ[n, m+1] * t)) *
+    (C * cos((m) * atan(x, y)) + D * sin((m) * atan(x, y)))
+
+exactB(x, y, t;)
+
+
+y = [exact(p[1], p[2], 0; m = 0, n = 2, A = 5, C = 1) for p in s[:point]];
+v = [0.0 for p in s[:point]];
+
+# θ = [atan(p[2] / p[1]) for p in s[:point]]
+# r = [sqrt(p[1]^2 + p[2]^2) for p in s[:point]]
+
+# Initial conditions
+
+I₀ = 1
+a_star = 1
+ω₀ = 5.5201
+ϕ = pi / 2
+
 eField(x) = begin
-    amp = (pi * r^2) * x[1]
-    amp * Point{3,Float32}(1, 0, 0)
+    r = sqrt(x[1]^2 + x[2]^2)
+    r_star = r / c
+    mag = [(-1 * μ₀ * c * pi / 2) * I₀ * ω₀ * a_star * Y₁ * J₁ * sin(ϕ)]
+    -[(μ₀ * c * pi / 2) * I₀ * ω₀ * a_star * J₁ * J₁ * cos(ϕ)]
+    mag * Point{3,Float32}(-y, x, 0)
 end
 
-E = ♭(sd, DualVectorField(eField.(sd[triangle_center(sd), :dual_point])))
-
-plot(first.(s[s[:src], :point]), E.data)
-
+EF = ♭(sd, DualVectorField(eField.(sd[triangle_center(sd), :dual_point])))
 B = TriForm(zeros(ntriangles(s)))
 
-u0 = vcat(B.data, E.data) # must be in order of presentation
-
-# Compress wiring diagram over contiguous matrix multiplications
-cont_dwd = deepcopy(dwd)
-Examples.contract_matrices!(cont_dwd, funcs)
-
-# Generate simulation function
-func, _ = gen_sim(cont_dwd, funcs, sd; autodiff = false);
-
-# Solve problem
-prob = ODEProblem(func, u0, (0.0, 0.5));
-sol = solve(prob, Tsit5());
+plot(first.(s[s[:src], :point]), EF.data)
 
 
-# Key for debugging simulation
-sim_key(dwd, orientation = LeftToRight)
-
+# Generate leapfrog simulation
+f1, f2, dwd1, dwd2 = gen_leapfrog(diag, funcs, sd, [:B, :E]);
+tspan = (0.0, 10.0)
+dyn_prob = DynamicalODEProblem(f1, f2, B, EF, tspan)
+sol = solve(dyn_prob, VerletLeapfrog(); dt = 0.01, progress = true, progress_steps = 100);
 
 exp_func, _ = gen_sim(dwd, funcs, sd; autodiff = false);
 
-# 
-fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol[10], 3)
+
+fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol[end-1], 6)
 fig
 
 
@@ -106,26 +110,65 @@ fig
 B_range = 1:ntriangles(s)
 E_range = 1:ne(s)
 
-fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol[10], 3; colorrange = (-1e-1, 1e-1))
+fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol(10.0), 3) #; colorrange = (-1e3, 1e3))
+ax.aspect = AxisAspect(1.0)
 fig
 
-
-# Record solution
-times = range(1e-4, sol.t[end], length = 500)
 
 # For visualizing E-field
 # colors = [sol(t)[E_range] for t in times]
 
-# For visualizing B-field
+# Record solution
+times = range(1e-4, sol.t[end], length = 300)
 colors = [vcat([[v, v, v] for v in sol(t)[B_range]]...) for t in times]
-
 colorrange = maximum(vcat(colors...))
 
 framerate = 30
 
-colorrange = (-1 * maximum(vcat(colors...)), maximum(vcat(colors...)))
-
-record(fig, "electric_field-radialDist_10.gif", collect(1:length(collect(times))); framerate = framerate) do i
+record(fig, "Bfield-disk10.gif", collect(1:length(collect(times))); framerate = framerate) do i
     ob.color = colors[i]
-    ob.colorrange = (-1 * maximum(vcat(colors...)), maximum(vcat(colors...)))
+    ob.colorrange = (-5, 5)
 end
+
+# Record 3D simulation
+# tn = Node(0.0)
+# sd2 = deepcopy(sd)
+# f(t) = begin
+#     new_points = [Point3{Float32}(p[1], 0, sol(t)[B_range[i]]) for (i, p) in enumerate(sd[triangle_center(sd), :dual_point])]
+#     sd2[triangle_center(sd2), :dual_point] .= new_points
+#     sd2
+# end
+
+# limits = FRect3D(Vec3f0(-1.0, -1.0, -1.0), Vec3f0(10.0, 10.0, 0.5))
+# fig, ax, ob = wireframe(lift(t -> f(t), tn); limits = limits)
+# i_range = range(0, 10.0, length = 100)
+# framerate = 30
+# record(fig, "EM_3D.gif", i_range; framerate = framerate) do i
+#     tn[] = i
+# end
+
+# t = 10.0
+# tn = Node(0.0)
+# val = zeros(nv(s))
+# norms = zeros(nv(s))
+# tris = triangle_vertices(s)
+# for i = 1:length(tris[1])
+#     for j = 1:3
+#         val[tris[j][i]] += sol(t)[i]
+#         norms[tris[j][i]] += 1
+#     end
+# end
+
+# f(t) = begin
+#     s2 = deepcopy(s)
+#     new_points = [eltype(s2[:point])(p[1], p[2], (val./norms)[i] * 4) for (i, p) in enumerate(s2[:point])]#=exact(p[1], p[2], t; m=0, n=2, A=5, C=1, c=10)=#
+#     s2[:point] .= new_points
+# end
+# limits = FRect3D(Vec3f0(-13.0, -13.0, -13.0), Vec3f0(13.0, 13.0, 13.0))
+# fig, ax, ob = wireframe(s2, limits = limits)
+
+# i_range = range(0, 10.0, length = 100)
+# framerate = 30
+# record(fig, "EM_3D.gif", i_range; framerate = framerate) do i
+#     tn[] = i
+# end
