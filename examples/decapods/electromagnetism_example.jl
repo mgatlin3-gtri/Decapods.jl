1b5776015ab0c3479b88937a129e87b9a6d9e1ca
Maia Gatlin <maia.gatlin@gtri.gatech.edu>
2021-12-02 17:41:51 -0500

    EM exact solution implementation updates

--- examples/decapods/electromagnetism_example.jl
+++ examples/decapods/electromagnetism_example.jl
@@ -27,13 +27,13 @@ using Logging: global_logger
     neg₁::Hom(Form1(X), Form1(X)) # -1
     E::Hom(munit(), Form1(X))     # electric field
     B::Hom(munit(), Form2(X))     # magnetic field
-    c::Hom(Form2(X), Form2(X))   # μ₀ / ϵ₀ (scalar)
+    c₀::Hom(Form2(X), Form2(X))   # μ₀ / ϵ₀ (scalar)
 end
 
 # Define Electromagnetic physics
 @present EM2D <: EM2DQuantities begin
     B ⋅ ∂ₜ(Form2(X)) == E ⋅ neg₁ ⋅ d₁(X)
-    E ⋅ ∂ₜ(Form1(X)) == B ⋅ c ⋅ ⋆₂(X) ⋅ dual_d₀(X) ⋅ ⋆₁⁻¹(X)
+    E ⋅ ∂ₜ(Form1(X)) == B ⋅ c₀ ⋅ ⋆₂(X) ⋅ dual_d₀(X) ⋅ ⋆₁⁻¹(X)
 end
 
 diag = eq_to_diagrams(EM2D)
@@ -49,86 +49,90 @@ sd = dual(s);
 
 # Define non-default operators (multiplication by constants)
 # c = -1 * (1.2566e-6 / 8.8542e-12)
-c = -3e8
+c₀ = -1
 
 funcs = sym2func(sd)
-funcs[:c] = Dict(:operator => c * I(ntriangles(s)), :type => MatrixFunc())
+funcs[:c₀] = Dict(:operator => c₀ * I(ntriangles(s)), :type => MatrixFunc())
 funcs[:neg₁] = Dict(:operator => -1 * I(ne(sd)), :type => MatrixFunc())
 
+μ₀ = 1.2566e-6
 
-# Bessel functions
+# Exact solutions for E and B (m = order of Bessel J function, n = order of Bessel Y function ω₀ = frequency, ϕ = phase, a_star = 1, c = speed, I₀ = current)
 
-exactE(x, y, t; m = 0, ω₀ = 0, A = 1, B = 0, c = sqrt(10), C = 1, D = 0) =
-    besselj(m, ω₀ * sqrt(x^2 + y^2)) *
-    (A * cos(c * λ[n, m+1] * t) + B * sin(c * λ[n, m+1] * t)) *
-    (C * cos((m) * atan(x, y)) + D * sin((m) * atan(x, y)))
+exactE(x, y, t; ω₀ = 0, ϕ = 0, a_star = 1, c = 0, I₀ = 1) =
+    [(-1 * c * pi / 2) * I₀ * ω₀ * a_star * bessely1(ω₀ * a_star) * besselj1(ω₀ * sqrt(x^2 + y^2) / c) * sin(ω₀ * t + ϕ)] -
+    [(c * pi / 2) * I₀ * ω₀ * a_star * besselj1(ω₀ * a_star) * besselj1(ω₀ * sqrt(x^2 + y^2) / c) * cos(ω₀ * t + ϕ)]
 
-exactB(x, y, t;)
+exactB(x, y, t; ω₀ = 0, ϕ = 0, a_star = 1, c = 0, I₀ = 1) =
+    [(-1 * pi / 2) * I₀ * ω₀ * a_star * bessely1(ω₀ * a_star) * besselj0(ω₀ * sqrt(x^2 + y^2) / c) * cos(ω₀ * t + ϕ)] +
+    [(pi / 2) * I₀ * ω₀ * a_star * besselj1(ω₀ * a_star) * besselj0(ω₀ * sqrt(x^2 + y^2) / c) * sin(ω₀ * t + ϕ)]
 
-
-y = [exact(p[1], p[2], 0; m = 0, n = 2, A = 5, C = 1) for p in s[:point]];
-v = [0.0 for p in s[:point]];
-
-# θ = [atan(p[2] / p[1]) for p in s[:point]]
-# r = [sqrt(p[1]^2 + p[2]^2) for p in s[:point]]
 
 # Initial conditions
 
-I₀ = 1
-a_star = 1
-ω₀ = 5.5201
-ϕ = pi / 2
+eField(v) = begin
+    r = sqrt(v[1]^2 + v[2]^2)
+    x = -1 * v[2] / r
+    y = v[1] / r
+    mag = exactE(x, y, 0; ω₀ = 5.5201, ϕ = 0, c = c₀)
+    mag[1] * Point{3,Float32}(x, y, 0)
+end
 
-eField(x) = begin
-    r = sqrt(x[1]^2 + x[2]^2)
-    r_star = r / c
-    mag = [(-1 * μ₀ * c * pi / 2) * I₀ * ω₀ * a_star * Y₁ * J₁ * sin(ϕ)]
-    -[(μ₀ * c * pi / 2) * I₀ * ω₀ * a_star * J₁ * J₁ * cos(ϕ)]
-    mag * Point{3,Float32}(-y, x, 0)
+bField(z) = begin
+    r = sqrt(z[1]^2 + z[2]^2)
+    x = z[1] / r
+    y = z[2] / r
+    mag = exactB(x, y, 0; ω₀ = 5.5201, ϕ = 0, c = c₀)
+    mag[1]
 end
 
 EF = ♭(sd, DualVectorField(eField.(sd[triangle_center(sd), :dual_point])))
-B = TriForm(zeros(ntriangles(s)))
+B = [bField(sd[sd[t, :tri_center], :dual_point]) for t = 1:ntriangles(s)]
 
-plot(first.(s[s[:src], :point]), EF.data)
+# EF = zeros(Float64, ne(s))
+# B = TriForm(zeros(ntriangles(s)))
+
+# linesegments(s, color=edges)
+# plot(first.(s[s[:src], :point]), EF.data)
 
 
 # Generate leapfrog simulation
 f1, f2, dwd1, dwd2 = gen_leapfrog(diag, funcs, sd, [:B, :E]);
-tspan = (0.0, 10.0)
+tspan = (0.0, 1.0)
 dyn_prob = DynamicalODEProblem(f1, f2, B, EF, tspan)
-sol = solve(dyn_prob, VerletLeapfrog(); dt = 0.01, progress = true, progress_steps = 100);
+sol = solve(dyn_prob, VerletLeapfrog(); dt = 0.0001, progress = true, progress_steps = 100);
 
-exp_func, _ = gen_sim(dwd, funcs, sd; autodiff = false);
-
-
-fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol[end-1], 6)
-fig
+print("boo")
+# exp_func, _ = gen_sim(dwd, funcs, sd; autodiff = false);
 
 
-# Plot solution
-B_range = 1:ntriangles(s)
-E_range = 1:ne(s)
-
-fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol(10.0), 3) #; colorrange = (-1e3, 1e3))
-ax.aspect = AxisAspect(1.0)
-fig
+# fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol[end-1], 6)
+# fig
 
 
-# For visualizing E-field
-# colors = [sol(t)[E_range] for t in times]
+# # Plot solution
+# B_range = 1:ntriangles(s)
+# E_range = 1:ne(s)
 
-# Record solution
-times = range(1e-4, sol.t[end], length = 300)
-colors = [vcat([[v, v, v] for v in sol(t)[B_range]]...) for t in times]
-colorrange = maximum(vcat(colors...))
+# fig, ax, ob = draw_wire(s, sd, dwd, exp_func, sol(10.0), 3) #; colorrange = (-1e3, 1e3))
+# ax.aspect = AxisAspect(1.0)
+# fig
 
-framerate = 30
 
-record(fig, "Bfield-disk10.gif", collect(1:length(collect(times))); framerate = framerate) do i
-    ob.color = colors[i]
-    ob.colorrange = (-5, 5)
-end
+# # For visualizing E-field
+# # colors = [sol(t)[E_range] for t in times]
+
+# # Record solution
+# times = range(1e-4, sol.t[end], length = 300)
+# colors = [vcat([[v, v, v] for v in sol(t)[B_range]]...) for t in times]
+# colorrange = maximum(vcat(colors...))
+
+# framerate = 30
+
+# record(fig, "Bfield-disk10.gif", collect(1:length(collect(times))); framerate = framerate) do i
+#     ob.color = colors[i]
+#     ob.colorrange = (-5, 5)
+# end
 
 # Record 3D simulation
 # tn = Node(0.0)
@@ -165,7 +169,7 @@ end
 #     s2[:point] .= new_points
 # end
 # limits = FRect3D(Vec3f0(-13.0, -13.0, -13.0), Vec3f0(13.0, 13.0, 13.0))
-# fig, ax, ob = wireframe(s2, limits = limits)
+# fig, ax, ob = wireframe(lift(t -> f(t), tn); limits = limits)
 
 # i_range = range(0, 10.0, length = 100)
 # framerate = 30
